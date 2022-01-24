53a03f05521bb4dc246426616e3016b57cfc766a
Jorge Garcia <jgarciam@gmail.com>
2022-01-24 16:33:21 -0300

    test

--- maven.groovy
+++ maven.groovy
@@ -1,69 +1,27 @@
-def mavenPipeline(){
-
-        stage("Compile"){
-            steps {
-                script {
-                    dir('src'){
-                        sh './mvnw clean compile -e'
-                    }
-                }
-            }
+def call(){
+    stage("Compile"){        
+        dir('src'){
+            sh './mvnw clean compile -e'
+        }            
+    }
+    stage("Test"){        
+        dir('src'){
+            sh './mvnw clean test -e'
+        }            
+    } 
+    stage("Jar"){
+        dir('src'){
+            sh './mvnw clean package -e'
+        }            
+    }
+    stage("Run"){
+        dir('src'){
+            sh 'nohup bash mvnw spring-boot:run &'
+            sleep(30)
         }
-        stage("Test"){
-            steps {
-                script {
-                    dir('src'){
-                        sh './mvnw clean test -e'
-                    }
-                }
-            }
-        }
-        stage("SonarQube analysis") {
-            steps {
-                script {
-                    dir('src'){
-                        def scannerHome = tool 'SonarScanner';
-                        withSonarQubeEnv('My SonarQube Server') {  
-                            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-maven -Dsonar.java.binaries=build/classes"
-                        }
-                    }
-                }
-            }
-        }        
-        stage("Jar"){
-            steps {
-                script {
-                    dir('src'){
-                        sh './mvnw clean package -e'
-                    }
-                }
-            }
-        }
-        stage("Upload Nexus") {
-            steps {
-                script {
-                    dir('src'){
-                        nexusArtifactUploader artifacts: [[artifactId: 'DevOpsUsach2020', file: 'build/DevOpsUsach2020-0.0.1.jar', type: 'jar']], credentialsId: 'nexus', groupId: 'com.devopsusach2020', nexusUrl: 'nexus:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'test-nexus', version: '0.0.1'
-                    }
-                }
-            }
-        }    
-        stage("Run"){
-            steps {
-                script {
-                    dir('src'){
-                        sh 'nohup bash mvnw spring-boot:run &'
-                        sleep(30)
-                    }
-                }
-            }
-        }
-        stage("Curl API"){
-            steps {
-                script {
-                    sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-                }
-            }
-        }
-
+    }
+    stage("Curl API"){
+        sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
 }
+return this
