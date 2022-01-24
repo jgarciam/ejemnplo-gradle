62d96a7154369311a1a45a8ad6e64a556fabf775
Jorge Garcia <jgarciam@gmail.com>
2022-01-24 16:44:50 -0300

    test

--- maven.groovy
+++ maven.groovy
@@ -1,24 +1,16 @@
 def call(){
     stage("Compile"){        
-        dir('src'){
-            sh './${env.JOB_NOME}/mvnw clean compile -e'
-        }            
+        sh './mvnw clean compile -e'   
     }
     stage("Test"){        
-        dir('src'){
-            sh './mvnw clean test -e'
-        }            
+        sh './mvnw clean test -e'
     } 
     stage("Jar"){
-        dir('src'){
-            sh './mvnw clean package -e'
-        }            
+        sh './mvnw clean package -e'    
     }
     stage("Run"){
-        dir('src'){
-            sh 'nohup bash mvnw spring-boot:run &'
-            sleep(30)
-        }
+        sh 'nohup bash mvnw spring-boot:run &'
+        sleep(30)
     }
     stage("Curl API"){
         sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
